a3771abd9ca67184334c774f983eea4a651f9a5a
alagarsamya8870 <42169898+alagarsamya8870@users.noreply.github.com>
2019-01-25 18:41:05 +0530

    Rename c to alagar.c

---
+++ alagar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char c;
+printf("enter the value");
+scanf("%c",$c);
+if((c>='a'&& c<='z')||(c>'A' && c<='Z'))
+{
+printf("%c alphebet",c);
+}
+else
+{
+printf("%c not",c);
+}
+return 0;
+}
